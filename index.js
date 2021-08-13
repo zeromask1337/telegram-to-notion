95fb25bc517741db904f4505324cb932b237adf2
Demonizer <vysmol@example.com>
2021-08-13 22:23:08 +0300

    - Implemented first iteration of regex text filtering and pushing to notion

--- index.js
+++ index.js
@@ -10,47 +10,48 @@ const url = require("url");
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
+const notionPush = async (postURL) => {
+  const blockId = "fd1e63d7d8864afb84c5e3c72ab1206b";
+  const response = await notion.blocks.children.append({
+    block_id: blockId,
+    children: [
+      {
+        object: "block",
+        type: "paragraph",
+        paragraph: {
+          text: [
+            {
+              type: "text",
+              text: {
+                content: postURL,
+                link: {
+                  url: postURL,
+                },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  });
+  console.log(response);
+};
+
 bot.command("start", (ctx) => {
   // Get text of new message
   console.log(`Incoming chat`, ctx.chat);
 });
 
-bot.on("message::url", (ctx) => {
+bot.on("message::url", async (ctx) => {
   // console.log(ctx.msg.caption_entities[0].url);
   const urlFilter =
     /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-  console.log(ctx.msg.text.match(urlFilter)); //TODO: add regix filter to url in caption
+  const link = await ctx.msg.text.match(urlFilter); //TODO: add regix filter to url in caption
+  await notionPush(link[0]);
+  // console.log("Successfully ?");
 });
 
 bot.start(console.log("Bot started..."));
-
-// (async () => {
-//   const blockId = "fd1e63d7d8864afb84c5e3c72ab1206b";
-//   const response = await notion.blocks.children.append({
-//     block_id: blockId,
-//     children: [
-//       {
-//         object: "block",
-//         type: "paragraph",
-//         paragraph: {
-//           text: [
-//             {
-//               type: "text",
-//               text: {
-//                 content:
-//                   "https://blog.bitsrc.io/web-authentication-cookies-vs-tokens-8e47d5a96d34",
-//                 link: {
-//                   url: "https://blog.bitsrc.io/web-authentication-cookies-vs-tokens-8e47d5a96d34",
-//                 },
-//               },
-//             },
-//           ],
-//         },
-//       },
-//     ],
-//   });
-//   console.log(response);
-// })();
 
 // bot.inlineQuery(/best bot (framework|library)/, async (ctx) => {
 //   await ctx.answerInlineQuery(
